7bd666b2e797ebc2abd8a8440379251293a240b9
Douglas Dalmoro <108308625+DouglasDalmoro@users.noreply.github.com>
2024-03-11 21:32:21 -0300

    Envio das telas relacionadas ao frontEnd para futura integração ao BackEnd

---
+++ Pdv/src/test/java/com/GaletoUniao/Pdv/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GaletoUniao.Pdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
